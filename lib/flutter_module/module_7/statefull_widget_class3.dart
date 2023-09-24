432f5ab48541ffee75c7229adb788aa4c19049e8
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-09-24 20:36:17 +0600

    updated

---
+++ lib/flutter_module/module_7/statefull_widget_class3.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterScreen(),
+    );
+  }
+}
+
+/// 1. Extends stateful widget
+/// 2. Create a State
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CounterState();
+}
+
+/// state
+class _CounterState extends State<CounterScreen> {
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Conter App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              counter.toString(),
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            ElevatedButton(onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      ProfileScreen(userName: 'Rabbi'),
+                ),
+              );
+            }, child: Text('Profile'))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter = counter + 1;
+          setState(() {});
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+// Widget class
+class ProfileScreen extends StatefulWidget {
+  final String userName;
+  const ProfileScreen({super.key, required this.userName});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+// state class -> widget class
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Text(widget.userName),
+      ),
+    );
+  }
+}
